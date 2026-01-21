60129a3afcb2ba3614a172d3e4935d9253421030
murilo.sousa <murilovfx10@gmail.com>
2026-01-21 09:13:44 -0300

    Criando uma função no botão

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let título = document.querySelector('h1') // Vai selecionar alguma coisa do HTML. "()" mostra o que vai ser escolhido.
 título.innerHTML = "Jogo do número secreto";
 
-let parágrafo = document.querySelector('´p')
+let parágrafo = document.querySelector('p')
 parágrafo.innerHTML = 'Escolha um número entre 1 e 10'
+
+function verificarChute() {
+    console.log('O botão foi clicado!')
+}
