a57c6bfe99d563a8e48a9680317cc3937f2e53ef
murilo.sousa <murilovfx10@gmail.com>
2026-01-21 09:05:20 -0300

    Primeiro commit do projeto

---
+++ app.js
@@ -0,0 +1,5 @@
+let título = document.querySelector('h1') // Vai selecionar alguma coisa do HTML. "()" mostra o que vai ser escolhido.
+título.innerHTML = "Jogo do número secreto";
+
+let parágrafo = document.querySelector('´p')
+parágrafo.innerHTML = 'Escolha um número entre 1 e 10'
